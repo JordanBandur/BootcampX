078d03fc363acac84985156ab4160e03b949317a
JordanBandur <jordanbandur@hotmail.ca>
2024-04-30 18:36:18 -0700

    Create query for most_confusing_assignments

---
+++ 4_queries/most_confusing_assignments.sql
@@ -0,0 +1,6 @@
+SELECT a.id, a.name, a.day, a.chapter,
+    count(ar.id) AS total_assistances
+FROM assignments a
+LEFT JOIN assistance_requests ar ON a.id = ar.assignment_id
+GROUP BY a.id, a.day, a.chapter, a.name
+ORDER BY total_assistances DESC;
